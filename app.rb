61d651f524ae967f620e0c2f4740c81d16bfe90d
Kai-Cai <kaiaschiff@gmail.com>
2018-06-12 17:46:48 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,5 +6,11 @@ class App < Sinatra::Base
     "My name is Kaia."
   end
   
+  get 'hometown' do
+    "My hometown is Denver, CO."
+  end
+  
+  get 'favorite-song' do
+    "My favorite song is If You're Over Me by Years & Years."
   
 end
