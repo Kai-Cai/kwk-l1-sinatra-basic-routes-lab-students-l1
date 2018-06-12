8922628fe91ec5d5122c2a5cd87d13fde238900d
Kai-Cai <kaiaschiff@gmail.com>
2018-06-12 17:52:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Kaia."
+  end
+  
+  get '/hometown' do
+    "My hometown is Denver, CO."
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is If You're Over Me by Years & Years."
+  end
+  
 end
